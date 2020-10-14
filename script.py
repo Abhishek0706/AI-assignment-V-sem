27376097d8fc2f2cb8e065ed2679a18b56ee51c7
as1ngh <anupam.1262000@gmail.com>
2020-10-14 15:45:58 +0530

    add frequency table, word and pos count table

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import csv
 import os
 import io
 from typing import TextIO
+from collections import defaultdict
 
 
 def findFile(path):
@@ -20,14 +21,68 @@ def to_parse(path):
     val_to_ret=[]
     for words in rootTree.iter('w'):
         val_to_ret.append([words.text,words.attrib.get('c5'),words.attrib.get('hw'),words.attrib.get('pos')])
-    ##print(val_to_ret)
+
     return val_to_ret
 
+def create_dict(wordList):
+    dict_word=defaultdict(dict)
+    dict_cnt_c5=defaultdict(int)
+    dict_cnt_word=defaultdict(int)
+    for x in wordList:
+        if x[0] in dict_word:
+            if x[1] in dict_word[x[0]]:
+                dict_word[x[0]][x[1]]+=1
+            else:
+                dict_word[x[0]][x[1]]=1
+        else:
+            dict_word[x[0]][x[1]]=1
+        dict_cnt_c5[x[1]]+=1
+        dict_cnt_word[x[0]]+=1
+    return [dict_word,dict_cnt_word,dict_cnt_c5];
+
+
+
 def main():
+
+    wordList=findFile('./Train-corups')
+    dict_list=create_dict(wordList)
+    frequency_table = []
+    wordCount=[];
+    posCount=[];
+
+    for key in dict_list[0]:
+        for c5 in dict_list[0][key]:
+            frequency_table.append([key,c5,dict_list[0][key][c5]])
+            ##print(key+" "+str(dict_list[0][key][c5]))
+
+    for key in dict_list[1]:
+        wordCount.append([key,dict_list[1][key]])
+
+    for key in dict_list[2]:
+        posCount.append([key,dict_list[2][key]])
+
+
     file: TextIO
     with io.open('./readableFormat.csv','w',encoding='utf-8') as file:
         writer=csv.writer(file)
-        writer.writerows(findFile('./Train-corups'))
+        writer.writerows(wordList)
+
+    file: TextIO
+    with io.open('./frequencyTable.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(frequency_table)
+
+    file: TextIO
+    with io.open('./wordCount.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(wordCount)
+
+    file: TextIO
+    with io.open('./posCount.csv', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(posCount)
+
+
 
 
 if __name__=='__main__':
